90d4c0a332cc04b231ee00128d921ce3fe8ababc
Yesterday17 <t@yesterday17.cn>
2019-03-31 16:24:43 +0800

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# BlueCompass
+# Blue Compass
+
 Fan-oriented minecraft mod.
+
+https://www.inoriminase.com/
+
+## Album Items
+
+- [ ] Innocent flower
+- [ ] Blue Compass
+- [ ] Catch The Rainbow
+
+## Single Items
+
+### 1st
+
+- [ ] 夢のつぼみ
+- [ ] 笑顔が似合う日
+- [ ] あの日の空へ
+
+### 2nd
+
+- [ ] harmony ribbon
+- [ ] Dreaming Girls
+- [ ] Ring of Smile
+
+### 3rd
+
+- [ ] Starry Wish
+- [ ] 夏の約束
+- [ ] [Melody Flag](mmf-site)
+
+[mmf-site]: http://melody-flag.com/
---
+++ src/main/java/cn/yesterday17/bluecompass/BlueCompass.java
@@ -0,0 +1,31 @@
+package cn.yesterday17.bluecompass;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = BlueCompass.MODID, name = BlueCompass.NAME, version = BlueCompass.VERSION)
+public class BlueCompass
+{
+    public static final String MODID = "bluecompass";
+    public static final String NAME = "Blue Compass";
+    public static final String VERSION = "1.0.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
